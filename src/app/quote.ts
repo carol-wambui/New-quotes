880cadb2026f1f13a5c46c4f1bea47edbff50541
carol-wambui <carolwambuidaystar@gmail.com>
2020-11-08 16:27:45 +0300

    created form-component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,19 @@
 export class Quote {
+    id:number;
+    author:string;
+    description:string;
+    user:string;
+    day:Date;
+    likes:number;
+    unlikes:number;
+    constructor(id:number,author:string,description:string,user:string,day:Date){
+        this.id=id;
+        this.author=author;
+        this. description=description;
+        this. user=user;
+        this.day=day;
+        this.likes=0;
+        this.unlikes=0;
+
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  
-
+  quotes: Quote[] = [
+    new Quote(1, 'Octavia E. Butler', 'You don’t start out writing good stuff. You start out writing crap and thinking it’s good stuff, and then gradually you get better at it', "Caro", new Date(2020, 11, 25)),
+    new Quote(2, 'Winston Churchill', 'Success is not final, failure is not fatal: it is the courage to continue that counts.', "Caro", new Date(2021, 1, 5)),
+    new Quote(3, 'Helen Keller', 'Never bend your head. Always hold it high. Look the world straight in the eye.', "Caro", new Date(2019, 7, 7)),
+    new Quote(4, 'Zig Ziglar', 'What you get by achieving your goals is not as important as what you become by achieving your goals.', "", new Date(2020, 12, 7)),
+    new Quote(5, 'Amy Poehler​​', 'Limit your "always" and your "nevers".', "benny", new Date(2018, 10, 13)),
+  ];
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  upvotes(index) {
+    this.quotes[index].likes++;
+  }
+  downvotes(index) {
+    this.quotes[index].unlikes++;
+  }
   constructor() { }
 
   ngOnInit(): void {
