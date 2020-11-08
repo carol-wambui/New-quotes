96ed31f6f8dd142ca0baebc90276b252f148160f
carol-wambui <carolwambuidaystar@gmail.com>
2020-11-08 22:22:57 +0300

    revome quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,8 +11,7 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'Octavia E. Butler', 'You don’t start out writing good stuff. You start out writing crap and thinking it’s good stuff, and then gradually you get better at it', "Caro", new Date(2020, 11, 25)),
     new Quote(2, 'Winston Churchill', 'Success is not final, failure is not fatal: it is the courage to continue that counts.', "Caro", new Date(2021, 1, 5)),
     new Quote(3, 'Helen Keller', 'Never bend your head. Always hold it high. Look the world straight in the eye.', "Caro", new Date(2019, 7, 7)),
-    new Quote(4, 'Zig Ziglar', 'What you get by achieving your goals is not as important as what you become by achieving your goals.', "", new Date(2020, 12, 7)),
-    new Quote(5, 'Amy Poehler​​', 'Limit your "always" and your "nevers".', "benny", new Date(2018, 10, 13)),
+  
   ];
   addNewQuote(quote) {
     let quoteLength = this.quotes.length;
