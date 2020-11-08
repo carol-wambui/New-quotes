66f08bfb1f0cbd2dedd1eb29bf845b08af2e3826
carol-wambui <carolwambuidaystar@gmail.com>
2020-11-08 23:56:57 +0300

    add README.md

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,20 +11,20 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'Octavia E. Butler', 'You don’t start out writing good stuff. You start out writing crap and thinking it’s good stuff, and then gradually you get better at it', "Caro", new Date(2020, 11, 25)),
     new Quote(2, 'Winston Churchill', 'Success is not final, failure is not fatal: it is the courage to continue that counts.', "Caro", new Date(2021, 1, 5)),
     new Quote(3, 'Helen Keller', 'Never bend your head. Always hold it high. Look the world straight in the eye.', "Caro", new Date(2019, 7, 7)),
-  
-  ];
+   
+  ];  
   addNewQuote(quote) {
     let quoteLength = this.quotes.length;
     quote.id = quoteLength + 1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
-  }
+  } 
   deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].description}?`)
 
       if (toDelete){
-        this.quotes.splice(index,1)
+        this.quotes.splice(index,1) 
       }
     }
   }
